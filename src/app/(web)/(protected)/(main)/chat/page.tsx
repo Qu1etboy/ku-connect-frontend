89d0221f70eb9433ec1a650074959bdcc541a810
NpatsL <napatsorn.lpt@gmail.com>
2025-03-28 18:02:10 +0700

    feat(chat): integrate pending interactions display in chat list

--- src/app/(web)/(protected)/(main)/chat/page.tsx
+++ src/app/(web)/(protected)/(main)/chat/page.tsx
@@ -2,21 +2,13 @@
 import MainLayout from "@/components/layout/main";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { listChat } from "@/services/chat";
+import { getPendingInteractions } from "@/services/interaction";
 import { formatChatTime } from "@/utils/date";
+import { getProfileImageUrl } from "@/utils/url";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 const MAX_DISPLAY = 4;
-
-const connectionRequest = {
-  total: 3,
-  avatars: [
-    "https://i.pravatar.cc/150?img=1",
-    "https://i.pravatar.cc/150?img=2",
-    "https://i.pravatar.cc/150?img=3",
-    "https://i.pravatar.cc/150?img=4",
-  ],
-};
 
 export default function ChatListPage() {
   const router = useRouter();
@@ -25,6 +17,12 @@ export default function ChatListPage() {
     queryKey: ["listChat"],
     queryFn: () => listChat(),
   });
+
+  const { data: pendingInteractions } = useQuery({
+    queryKey: ["pendingInteractions"],
+    queryFn: () => getPendingInteractions(),
+  });
+  console.log("pendingInteractions", pendingInteractions);
 
   const handleChatRoomClick = (chatId: string) => {
     router.push(`/chat/${chatId}`);
@@ -38,30 +36,46 @@ export default function ChatListPage() {
         {/* Friends */}
         <div className="flex items-center space-x-4 px-4 py-7">
           {/* Avatar */}
-          <div className="flex -space-x-5">
-            {connectionRequest.avatars
-              .slice(0, MAX_DISPLAY)
-              .map((avatar, index) => (
-                <Avatar key={index} className="h-10 w-10">
-                  <AvatarImage src={avatar} alt="avatar" />
-                  <AvatarFallback>U</AvatarFallback>
-                </Avatar>
-              ))}
-            {/* More */}
-            {connectionRequest.total > MAX_DISPLAY && (
-              <div className="z-10 flex h-10 w-10 items-center justify-center rounded-full bg-gray-200">
-                <p className="text-sm font-semibold text-gray-500">
-                  {connectionRequest.total > 99 + MAX_DISPLAY
-                    ? "99+"
-                    : `+${connectionRequest.total - MAX_DISPLAY}`}
-                </p>
-              </div>
-            )}
-          </div>
+          {(pendingInteractions?.count ?? 0) > 0 ?(
+            <div className="flex -space-x-5">
+              {pendingInteractions?.pendingInteractions
+                .slice(0, MAX_DISPLAY)
+                .map((interaction, index) => (
+                  <Avatar key={index} className="h-10 w-10">
+                    <AvatarImage
+                      src={getProfileImageUrl(interaction.image ?? "")}
+                      alt="avatar"
+                    />
+                    <AvatarFallback>U</AvatarFallback>
+                  </Avatar>
+                ))}
+              {/* More */}
+              {(pendingInteractions?.count ?? 0) > MAX_DISPLAY && (
+                <div className="z-10 flex h-10 w-10 items-center justify-center rounded-full bg-gray-200">
+                  <p className="text-sm font-semibold text-gray-500">
+                    {(pendingInteractions?.count ?? 0) > 99 + MAX_DISPLAY
+                      ? "99+"
+                      : `+${(pendingInteractions?.count ?? 0) - MAX_DISPLAY}`}
+                  </p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <Avatar className="h-10 w-10">
+              <AvatarImage src="/ku-connect-bg-white.svg" alt="avatar" />
+              <AvatarFallback>U</AvatarFallback>
+            </Avatar>
+          )}
           {/* Text */}
-          <p className="text-base font-semibold">
-            {connectionRequest.total} People want to be friend
-          </p>
+          {(pendingInteractions?.count ?? 0) > 0 ? (
+            <p className="text-base font-semibold">
+              {pendingInteractions?.count ?? 0} People want to be friend
+            </p>
+          ) : (
+            <p className="text-base font-semibold">
+              1 People want to be friend
+            </p>
+          )}
         </div>
         {/* Chat */}
         <p className="px-2 pb-1 text-sm font-semibold">Your Friends</p>
